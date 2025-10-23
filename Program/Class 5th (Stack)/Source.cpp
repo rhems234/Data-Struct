8e756776247893dcd5cfd53537149170ff97cefd
rhems234 <rhems234@gmail.com>
2025-10-23 19:46:11 +0900

    (2025.10.23) 원형 큐

---
+++ Program/Class 5th (Stack)/Source.cpp
@@ -0,0 +1,112 @@
+﻿#include <iostream>
+
+using namespace std;
+
+template <typename T>
+class Stack {
+private:
+    int capacity;
+    int highset;
+    T* container;
+
+public:
+
+    Stack() {
+        capacity = 0;
+        highset = -1;
+        container = nullptr;
+    }
+
+    void resize(int newsize) {
+        // 1. capactiy에 새로운 size 값을 저장합니다.
+        capacity = newsize;
+
+        // 2. 새로운 포인터 변수를 생성해서 새롭게 만들어진 메모리 공간을 가리키도록 합니다.
+        T* newpointer = new T[capacity];
+
+        // 3. 새로운 메모리 공간의 값을 초기화합니다.
+        for (int i = 0; i < capacity; i++) {
+            newpointer[i] = NULL;
+        }
+
+        // 4. 기존 배열에 있는 값을 복사해서 새로운 배열에 넣어줍니다.
+        for (int i = 0; i < highset + 1; i++) {
+            newpointer[i] = container[i];
+        }
+
+
+        // 5. 기본 배열의 메모리를 해제합니다.
+        delete[] container;
+
+        // 6. 기존 배열을 가리키던 포인터 변수의 값을 새로운 배열의 시작 주소로 가리킵니다.
+        container = newpointer;
+    }
+
+    void push(T data) {
+
+        if (capacity <= 0) {
+            resize(1);
+        }
+        else if (highset + 1 >= capacity) {
+            resize(capacity * 2);
+        }
+
+        //cout << "result : " << data << endl;
+
+        highset++;
+
+        container[highset] = data;
+
+    }
+
+    const T& top() {
+        return container[highset];
+    }
+
+    const int& size() {
+        return highset + 1;
+    }
+
+    const bool& empty() {
+        return (highset <= -1);
+    }
+
+    void pop() {
+        if (empty()) {
+            cout << "Stack is empty" << endl;
+            return;
+        }
+        else {
+            highset--;
+        }
+    }
+
+    ~Stack() {
+        if (container != nullptr) {
+            delete[] container;
+        }
+    }
+};
+
+int main()
+{
+
+    Stack<int> stack;
+
+    stack.push(10);
+    stack.push(20);
+    stack.push(30);
+    stack.push(40);
+    stack.push(50);
+
+    while (stack.empty() == false) {
+        cout << stack.top() << endl;
+
+        stack.pop();
+    }
+
+    cout << "Stack size : " << stack.size() << endl;
+
+
+    return 0;
+}
--- Program/Program/Program.cpp
+++ Program/Program/Program.cpp
@@ -2,58 +2,61 @@
 
 using namespace std;
 
+#define SIZE 5
+
+// Queue -> First In First Out
 template <typename T>
-class Stack {
-private :
-    int capacity;
-    int highset;
-    T* container;
+class Queue {
 
-public :
+private:
+    int front;
+    int rear;
+    T container[SIZE];
 
-    Stack() {
-        capacity = 0;
-        highset = -1;
-        container = nullptr;
-    }
-
-    void resize(int newsize) {
-        // 1. capactiy에 새로운 size 값을 저장합니다.
-        capacity = newsize;
-
-        // 2. 새로운 포인터 변수를 생성해서 새롭게 만들어진 메모리 공간을 가리키도록 합니다.
-        T* newpointer = new T[capacity];
-
-        // 3. 새로운 메모리 공간의 값을 초기화합니다.
-        for (int i = 0; i < capacity; i++) {
-            newpointer[i] = NULL;
+public:
+    Queue() {
+        front = 0;
+        rear = 0;
+        
+        for (int i = 0; i < SIZE; i++) {
+            container[i] = NULL;
         }
-
-        // 4. 기존 배열에 있는 값을 복사해서 새로운 배열에 넣어줍니다.
-        for (int i = 0; i < highset; i++) {
-            newpointer[i] = container[i];
-        }
-
-
-        // 5. 기본 배열의 메모리를 해제합니다.
-        delete[] container;
-
-        // 6. 기존 배열을 가리키던 포인터 변수의 값을 새로운 배열의 시작 주소로 가리킵니다.
-        container = newpointer;
     }
 
     void push(T data) {
-        
-        if (capacity == 0) {
-            resize(1);
-        }
-        else if (highset + 1 >= capacity) {
-            resize(capacity * 2);
+        if (rear == SIZE) {
+            cout << "Linear Queue overflow" << endl;
+            return;
         }
 
-        highset++;
-        container[highset] = data;
+        container[rear++] = data;
 
+    }
+
+    void pop() {
+        if (empty()) {
+            cout << "Queue is Empty" << endl;
+        }
+        else {
+            //cout << "delete : " << container[front] << endl;
+
+            container[front] = NULL;
+            front++;
+        }
+    }
+
+    const bool& empty() {
+        return front == rear;
+    }
+
+    // 가장 앞 데이터 출력
+    const T& peek() {
+        if (empty()) {
+            cout << "Queue is Empty" << endl;
+            exit(1);
+        }
+
+        return container[front];
     }
 
 };
@@ -61,11 +64,18 @@ public :
 int main()
 {
 
-    Stack<int> stack;
+    Queue<int> queue;
 
-    stack.push(10);
-    stack.push(20);
-    stack.push(30);
+    queue.push(10);
+    queue.push(20);
+    queue.push(30);
+    queue.push(40);
+    queue.push(50);
+
+    queue.pop();
+    queue.pop();
+    queue.pop();
+
 
     return 0;
 }
