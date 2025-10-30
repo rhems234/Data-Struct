e65c3e6783f6b83e8c7f34f1c21f60a9ad89e206
rhems234 <rhems234@gmail.com>
2025-10-30 19:46:23 +0900

    (2025.10.30) 인접 그래프

--- Program/Program/Program.cpp
+++ Program/Program/Program.cpp
@@ -54,21 +54,83 @@ public:
     }
 
     void insert(KEY key, VALUE value) {
+        // 해시 함수를 통해서 저장하는 임시 변수.
         unsigned int index = hash_function(key);
 
+        // 새로운 노드 생성.
         Node* newNode = new Node;
         newNode->key = key;
         newNode->value = value;
         newNode->next = nullptr;
 
-        if (newNode->value >= 1) {
-            cout << "Insert complete" << endl;
+        // 노드가 1개라도 존재하지 않는다면
+        if (bucket[index].count == 0) {
+            bucket[index].head = newNode;
+        }
+        else {
             newNode->next = bucket[index].head;
             bucket[index].head = newNode;
-
-            bucket[index].count++;
         }
+        // bucket[index]의 count를 증가시킵니다.
+        bucket[index].count++;
+    }
 
+    void erase(KEY key) {
+        // 해시 함수를 통해서 값을 받는 임시 변수.
+        int index = hash_function(key);
+
+        // Node를 탐색할 수 있는 포인터 변수.
+        Node* currentNode = bucket[index].head;
+
+        // 이전 Node를 저장 할 수 있는 포인터 변수 선언.
+        Node* prevNode = nullptr;
+
+        // currentNode가 nullptr과 같다면 함수를 종료합니다.
+        if (prevNode == nullptr) {
+            cout << "Not key found..." << endl;
+        } 
+        else {
+           // currentNode를 이용해서 내가 찾고자 하는 key값을 찾습니다.
+            while (currentNode != nullptr) 
+            {
+                if (currentNode->key == key) 
+                {
+                    if (currentNode == bucket[index].head) 
+                    {
+                        bucket[index].head = currentNode->next;
+                    }
+                    else 
+                    {
+                        prevNode->next = currentNode->next;
+                    }
+
+                    size--;
+
+                    bucket[index].count--;
+
+                    delete currentNode;
+
+                    return;
+                }
+                else {
+                    prevNode = currentNode;
+
+                    currentNode = currentNode->next;
+                }
+            }
+
+            cout << "not key found..." << endl;
+        }
+    }
+
+    // 밀집도
+    const float& load_factor() {
+        return (float)size / capacity;
+    }
+
+    // 버킷 갯수
+    const int& bucket_count() {
+        return capacity;
     }
 
 };
@@ -77,14 +139,19 @@ int main()
 {
     HashTable<const char *, int> hashtable;
 
-    cout << hashtable.hash_function(3) << endl;
+    /*cout << hashtable.hash_function(3) << endl;
     cout << hashtable.hash_function(4) << endl;
-    cout << hashtable.hash_function(5) << endl;
+    cout << hashtable.hash_function(5) << endl;*/
 
-    hashtable.insert("1", 0);
-    hashtable.insert("2", 1);
-    hashtable.insert("3", 2);
-    hashtable.insert("4", 3);
+    hashtable.insert("apple", 10);
+    hashtable.insert("banana", 20);
+    hashtable.insert("cherry", 30);
+    
+    hashtable.erase("melon");
+    hashtable.erase("apple");
+
+    cout << hashtable.load_factor() << endl;
+    cout << hashtable.bucket_count() << endl;
 
     return 0;
 }
